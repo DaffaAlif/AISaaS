66b6e85733f0756b89e84c56598e4e7501680305
DaffaAlif <daffalif02@gmail.com>
2024-10-27 09:36:52 +0700

    user model

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,27 @@
+import { model, models, Schema } from "mongoose";
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+  clerkId: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  planId?: string;
+  creditBalance: number;
+}
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  photo: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String},
+  planId: { type: Number },
+  creditBalance: { type: Number, default : 10},
+});
+
+const User = models?.User || model("User", UserSchema);
+
+export default User;
